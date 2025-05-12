071c6313f978b8417e946d4971ec791ed28e5c22
kutah1 <jameskutah47@gmail.com>
2025-05-12 12:41:43 +0100

    I have added a new file named `Tasks.jsx` in the `app/screens` directory. This file contains the implementation of the Tasks screen for the application. The screen includes a header,  a list of tasks, and a button to add new tasks.

---
+++ app/screens/Tasks.jsx
@@ -0,0 +1,25 @@
+import { View, Text,StyleSheet, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Tasks = () => {
+  return (
+    <SafeAreaView>
+      <View>
+           <Text> My Tasks</Text>
+        <View>
+
+        </View>
+        <View>
+
+        </View>
+      </View>
+    
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+}})
+export default Tasks
