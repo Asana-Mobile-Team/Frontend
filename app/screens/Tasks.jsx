9438db45213fbdf3427a89d56d35d0f6c564ad5b
kutah1 <jameskutah47@gmail.com>
2025-05-12 13:10:58 +0100

    Installed tailwindcss and postcss for styling Visit tailwindcss website to learn. It speeds up CSS styling.

--- app/screens/Tasks.jsx
+++ app/screens/Tasks.jsx
@@ -1,11 +1,16 @@
 import { View, Text,StyleSheet, SafeAreaView } from 'react-native'
-import React from 'react'
+
 
 const Tasks = () => {
   return (
     <SafeAreaView>
       <View>
-           <Text> My Tasks</Text>
+           <Text style={{alignItems:'center'}}> My Tasks
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style={{width: 20, height: 20}}>
+           <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+           </svg>
+
+           </Text>
         <View>
 
         </View>
